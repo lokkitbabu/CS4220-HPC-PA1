92d586d504d5e60de09607d8c05258f5326477f2
Lokkit <jlokkitsanjay@gmail.com>
2026-02-11 05:40:36 +0000

    uploading given docs

---
+++ PA1_Code/pi_calc.cpp
@@ -0,0 +1,11 @@
+#include "pi.h"
+
+double pi_calc(long int n, int rank, int size){
+    // Write your code below
+    ////////////////////////////////////////
+    
+
+
+    ////////////////////////////////////////
+    return 0.0;
+}
